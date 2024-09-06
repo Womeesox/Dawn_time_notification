31df636d9ac4c6c51e7561ef7c8ca08f14e65f2c
Socks <jankwiecien06@gmail.com>
2024-09-06 07:08:16 +0200

    Syncing between devices

--- main.py
+++ main.py
@@ -1,37 +1,75 @@
 import os
 import requests
+
+from dotenv import load_dotenv
+from json import load as json_load, JSONDecodeError
+
+from time import sleep
+
+from datetime import datetime, timedelta
 from timezonefinder import TimezoneFinder
 from time import sleep
-from dotenv import load_dotenv
-from pushbullet import PushBullet
-from pytz import timezone
+from pytz import utc, timezone
 
-# load_dotenv()
-# API_KEY = os.getenv("PUSHBULLET_API_KEY")
-# pb = PushBullet(API_KEY)
+from pushbullet import Pushbullet
 
-#for cyvil dawn that is time when it's actually light outside
+load_dotenv()
+API_KEY = os.getenv("PUSHBULLET_API_KEY")
+pb = Pushbullet(API_KEY)
+
+try:
+    config_file_path = "config.json"
+    with open(config_file_path, "r") as file:
+        config = json_load(file)
+
+    latitude = config["latitude"]
+    longitude = config["longitude"]
+
+    if latitude is None or longitude is None:
+        raise ValueError("Latitude and longitude must be specified in the config file.")
+except FileNotFoundError:
+    print(f"Error: The file {config_file_path} does not exist.")
+except JSONDecodeError:
+    print("Error: The config file is not a valid JSON.")
+except ValueError as e:
+    print(f"Error: {e}")
+
+tf = TimezoneFinder()
+local_timezone = timezone(tf.timezone_at(lat=latitude, lng=longitude))
+
+
+#for civil dawn that is time when it's actually light outside
 def get_dawn_time(lat, lng):
     url = "https://api.sunrise-sunset.org/json"
 
     params = {
-        'lat': lat,
-        'lng': lng,
-        'formatted': 0  # 0 for ISO 8601 format, which includes the full date and time
+        "lat": lat,
+        "lng": lng,
+        "formatted": 0  # 0 for ISO 8601 format, which includes the full date and time
     }
 
     response = requests.get(url, params=params)
 
     if response.status_code == 200:
         data = response.json()
-        dawn_time = data['results']['civil_twilight_begin']
-        return dawn_time
+        dawn_time = data["results"]["civil_twilight_begin"]
+        
+        utc_time = datetime.fromisoformat(dawn_time.replace("Z", "+00:00")) # parse to datetime object
+        local_time = utc_time.astimezone(local_timezone)
+
+
+        return local_time
     else:
-        return None
+        return None  
 
-#Warsaw, Poland    
-latitude = 52.229675
-longitude = 21.012230 
+while(True):
+    now = datetime.now()
+    dawn_time = get_dawn_time(latitude, longitude)
 
-# print(type(get_dawn_time(latitude, longitude)))
-print(get_time_zone(latitude, longitude))
+    if dawn_time < now:
+        dawn_time += timedelta(days=1)
+
+    time_diff = (dawn_time - now).total_seconds()
+
+    sleep(time_diff)
+    push = pb.push_note("It's light outside! 🥳", "Time to get some sunlight 🌞")
